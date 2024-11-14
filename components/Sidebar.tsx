6f436d6f804c0f035b4591b6e3abd131d9e87bc7
Dilajohn <okujaemmanuel97@gmail.com>
2024-11-14 19:27:12 +0000

    modify app (auth),app(root) and sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -19,7 +19,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
             src="/icons/logo.svg"
             width={34}
             height={34}
-            alt="MoFinance logo"
+            alt="TrustVault logo"
             className="size-[24px] max-xl:size-14"
           />
           <h1 className="sidebar-logo">TrustVault</h1>
